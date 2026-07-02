147213e7ec6e487a6a69881e8f1a7ccd540189a8
gp-kobayashi <kobayashi_y@gp1000.jp>
2026-07-02 16:07:43 +0900

    [add]headerの作成

---
+++ components/header/Header.tsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>oto_meishi</h1>
+      <nav className={styles.nav} aria-label="main navigation">
+        <a href="/login">ログイン</a>
+        <a href="/register">登録</a>
+        <a href="/help">ヘルプ</a>
+        <a href="/terms">利用規約</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
