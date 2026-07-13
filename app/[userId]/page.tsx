942d9b9e8d94ee4924ee49b1c39cc896e6c5764b
gp-kobayashi <kobayashi_y@gp1000.jp>
2026-07-13 14:30:38 +0900

    [feature][UserId]ページをdemoデータからsupabase接続のデータへの変更

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 import Card from "../../components/card/Card";
-import demoProfile from "../../lib/mock/profile";
 import styles from "./page.module.css";
+import { prisma } from "../../lib/prisma";
 import type { ProfileData } from "../../lib/mock/profileData";
 
 const UserIdPage = async ({
@@ -10,14 +11,21 @@ const UserIdPage = async ({
   params: Promise<{ userId: string }>;
 }) => {
   const { userId } = await params;
-  const link = demoProfile as ProfileData;
+  const profile = await prisma.profile.findUnique({
+    where: { userId },
+    include: { sns: true },
+  });
+
+  if (!profile) {
+    notFound();
+  }
 
   return (
     <main className={styles.main}>
       <a href="/" className={styles.logo}>
         <Image src="/TitleLogo.svg" alt="Logo" width={160} height={160} />
       </a>
-      <Card link={link} />
+      <Card link={profile as ProfileData} />
     </main>
   );
 };
